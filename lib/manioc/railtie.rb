395e8c22a82d5d3d02ac022232ff5e33e113de69
James Dabbs <jamesdabbs@gmail.com>
2017-08-11 18:23:09 -0400

    Add ActiveSupport::Reloader support
    `reload!` should now reset the container in a Rails console

--- lib/manioc/railtie.rb
+++ lib/manioc/railtie.rb
@@ -20,11 +20,23 @@ module Manioc
       _app.config.container = Manioc::Container.new
     end
 
+    reset = ->{ _app.config.container.reset! }
+
     config.to_prepare do
       if _app.config.eager_load
         _app.config.container.preload!
       else
-        _app.config.container.reset!
+        reset.call
+      end
+    end
+
+    if ActiveSupport.const_defined?(:Reloader) && ActiveSupport::Reloader.respond_to?(:to_complete)
+      ActiveSupport::Reloader.to_complete do
+        reset.call
+      end
+    elsif ActionDispatch.const_defined?(:Reloader) && ActionDispatch::Reloader.respond_to?(:to_cleanup)
+      ActionDispatch::Reloader.to_cleanup do
+        reset.call
       end
     end
   end
