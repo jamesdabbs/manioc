6085d8f7aba5e94c85b2dc9d0c1026d2038934b3
James Dabbs <jamesdabbs@gmail.com>
2017-12-08 15:00:19 -0500

    Add Env#fetch

--- lib/manioc/env.rb
+++ lib/manioc/env.rb
@@ -2,12 +2,20 @@ module Manioc
   class Env
     Unset = Class.new StandardError
 
+    def fetch key
+      ENV[key] || raise(Unest, key)
+    end
+
+    def [] key
+      ENV[key]
+    end
+
     def method_missing name, *args
       super if args.count > 1
       if name =~ /\A(.*)!\Z/
-        ENV[$1] || raise(Unset, $1)
+        fetch $1
       else
-        ENV[name.to_s]
+        self[name.to_s]
       end
     end
   end
